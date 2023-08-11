b694a15e2e9bf1d77d80b7379dce4dbd0f579e3e
Eduard Sysoev <krikisosedey@bk.ru>
2023-08-11 20:18:58 +0300

    add changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+string isWeekend = (dayOfWeek > 0 && dayOfWeek < 8) 
+    ? (dayOfWeek == 6 || dayOfWeek == 7) ? "выходной" : "не выходной"
+    : "это не день недели";
+
+Console.WriteLine($"{dayOfWeek} -> {isWeekend}");
+
